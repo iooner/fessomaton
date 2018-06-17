54fd1839c176783252fa923d41bb62c653547b2a
iooner <contact@iooner.be>
2018-06-17 23:29:59 +0200

    update

--- python PIL/imgblender.py
+++ python PIL/imgblender.py
@@ -1,15 +1,27 @@
 from PIL import Image, ImageEnhance
+import time, datetime, random
+outname = str(datetime.datetime.fromtimestamp(time.time()).strftime('%Y-%m-%d-%H-%M-%S'))
 
-alpha = 0.8
+alpha = 0.7
+hover_list = ['hover_1.jpg']
+i = random.randint(0, len(hover_list) - 1)  
+hover_file = hover_list[i] 
 
-# Conversion de la photo en NB
+
 camera_source = Image.open("testface.jpg")
-scan_hover = Image.open("hover_1.jpg")
-camera_source= vintage.vintage_colors(camera_source)
+watermark = Image.open("watermark.png")
+source_width, source_height = camera_source.size
+watermark_width, watermark_height = watermark.size
+
+scan_hover = Image.open(hover_file)
 camera_source = ImageEnhance.Contrast(camera_source).enhance(1.5)
 camera_source = ImageEnhance.Brightness(camera_source).enhance(1.5)
 
-#out = Image.blend(camera_source,scan_hover,alpha)
-#out = out.convert('1')
+
+out = Image.blend(camera_source,scan_hover,alpha)
+out = out.convert('1')
+out.paste(watermark, (source_width - watermark_width - 25, source_height - watermark_height - 25))
 out.show()
-out.save('testfaceNB.jpg')
+#imprimer ici avec cups
+out.save('allfiles/%s.jpg' % outname, format="JPEG")
+out.save('batch/%s.jpg' % outname, format="JPEG")
